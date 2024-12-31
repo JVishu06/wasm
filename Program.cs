a89ccc1cf13d521e11a16a2e1c14a401e87e073d
JVishu06 <jvishu.ai@gmail.com>
2024-12-31 19:05:29 +0530

    second last

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using wasm;
-using wasm.Services;
+using wasm.Sevices;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
